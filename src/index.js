55a04d817246d532e5f9ba62f611e7c9eded077c
Jonathan Won <jabinx92@gmail.com>
2020-12-01 14:38:41 -0800

    add first project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import BirthdayReminders from './BirthdayReminders';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BirthdayReminders />
   </React.StrictMode>,
   document.getElementById('root')
 );
