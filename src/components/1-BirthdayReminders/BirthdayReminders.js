b35032bb37ee4941031d5eeb0420dd1a598c9742
Jonathan Won <jabinx92@gmail.com>
2020-12-11 15:55:25 -0800

    birthdays today

--- src/components/1-BirthdayReminders/BirthdayReminders.js
+++ src/components/1-BirthdayReminders/BirthdayReminders.js
@@ -7,7 +7,7 @@ function BirthdayReminders() {
   return (
     <main>
       <section className='container'>
-        <h3>{people.length} birthdays today</h3>
+        <h3>{people.length} birthdays today!</h3>
         <List people={people} />
         <button onClick={() => setPeople([])}>clear all</button>
       </section>
