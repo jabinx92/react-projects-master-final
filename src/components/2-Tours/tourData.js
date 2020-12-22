5f5301ff811359ce60091f8cb9d50c6309405660
Jonathan Won <jabinx92@gmail.com>
2020-12-21 16:07:06 -0800

    fix paris image

--- src/components/2-Tours/tourData.js
+++ src/components/2-Tours/tourData.js
@@ -3,7 +3,7 @@ const tourData = [
         "id":"rec6d6T3q5EBIdCfD",
         "name":"Best of Paris in 7 Days Tour",
         "info":"Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!",
-        "image":"./images/tours/paris.jpeg",
+        "image":"./images/tours/paris.jpg",
         "price":"1,995"
     },{
         "id":"recIwxrvU9HfJR3B4","name":"Best of Ireland in 14 Days Tour",
